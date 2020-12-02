fc1432d5e59a3769ca80d749f3819d6e44b72028
Denis <denis.artyomov@gmail.com>
2020-12-02 22:56:34 +0500

    tests

--- tests/micro_format_tests.cpp
+++ tests/micro_format_tests.cpp
@@ -12,6 +12,8 @@ static bool print_to_str_callback(void* data, char character)
 	return true;
 }
 
+static int errors_count = 0;
+
 template <typename ... Args>
 void test_eq(const char *desired, const char *format, const Args& ... args)
 {
@@ -23,6 +25,7 @@ void test_eq(const char *desired, const char *format, const Args& ... args)
 
 	if (!ok)
 	{
+		errors_count++;
 		printf("ERROR: \"%s\" -> \"%s\"\n", desired, result.c_str());
 
 		// for debuging purposes
@@ -43,50 +46,78 @@ static void test_common()
 
 static void test_integer()
 {
-	test_eq("42",  "{}",  42);
-	test_eq("-42", "{}", -42);
+	// decimal
 
-	test_eq("+42", "{:+}", 42);
-	test_eq("-42", "{:+}", -42);
-	test_eq("+42", "{:+}", 42U);
-	test_eq("42",  "{:-}", 42);
-	test_eq("-42", "{:-}", -42);
+	test_eq("42",     "{}",  42);
+	test_eq("-42",    "{}", -42);
+	test_eq("+42",    "{:+}", 42);
+	test_eq("-42",    "{:+}", -42);
+	test_eq("+42",    "{:+}", 42U);
+	test_eq("42",     "{:-}", 42);
+	test_eq("-42",    "{:-}", -42);
+	test_eq("   42",  "{:5}", 42);
+	test_eq("  -42",  "{:5}", -42);
+	test_eq("  +42",  "{:+5}", 42);
+	test_eq("  -42",  "{:+5}", -42);
+	test_eq("00042",  "{:05}", 42);
+	test_eq("-0042",  "{:05}", -42);
+	test_eq("+0042",  "{:+05}", 42);
+	test_eq("000123", "{:06}", 123);
+	test_eq("-00123", "{:06}", -123);
+	test_eq("-00123", "{:+06}", -123);
+	test_eq("+00123", "{:+06}", 123);
+	test_eq(" 00123", "{: 06}", 123);
+	test_eq("-00123", "{: 06}", -123);
+	test_eq(" 123",   "{: }", 123);
+	test_eq("-123",   "{: }", -123);
 
-	test_eq("   42", "{:5}", 42);
-	test_eq("  -42", "{:5}", -42);
 
-	test_eq("  +42", "{:+5}", 42);
-	test_eq("  -42", "{:+5}", -42);
+	// hex
 
-	test_eq("00042", "{:05}", 42);
-	test_eq("-0042", "{:05}", -42);
-	test_eq("+0042", "{:+05}", 42);
-
-	test_eq("a", "{:x}", 0xa);
-	test_eq("A", "{:X}", 0xa);
-
-	test_eq("55553333", "{:x}", 0x55553333);
-	test_eq("0x55553333", "{:#x}", 0x55553333);
-	test_eq("0X55553333", "{:#X}", 0x55553333);
-
-	test_eq("-0x55553333", "{:#x}", -0x55553333);
-
-	test_eq("  0x123", "{:#7x}", 0x123);
-	test_eq(" -0x123", "{:#7x}", -0x123);
-	test_eq("-0x0123", "{:#07x}", -0x123);
-	test_eq("0x00123", "{:#07x}", 0x123);
-	test_eq("0000123", "{:07x}", 0x123);
-
+	test_eq("a",        "{:x}", 0xa);
+	test_eq("A",        "{:X}", 0xa);
+	test_eq("5533",     "{:x}", 0x5533);
+	test_eq("0x5533",   "{:#x}", 0x5533);
+	test_eq("0X5533",   "{:#X}", 0x5533);
+	test_eq("-0x5533",  "{:#x}", -0x5533);
+	test_eq("  0x123",  "{:#7x}", 0x123);
+	test_eq(" -0x123",  "{:#7x}", -0x123);
+	test_eq("-0x0123",  "{:#07x}", -0x123);
+	test_eq("0x00123",  "{:#07x}", 0x123);
+	test_eq("0000123",  "{:07x}", 0x123);
 	test_eq("123    ",  "{:<7x}", 0x123);
 	test_eq("123    ",  "{:<7x}", 0x123);
 	test_eq("0x123  ",  "{:<#7x}", 0x123);
 	test_eq("  123  ",  "{:^7x}", 0x123);
 	test_eq("  123   ", "{:^8x}", 0x123);
+	test_eq("  -123  ", "{:^8x}", -0x123);
+
+
+	// binary
+
+	test_eq("11001100",    "{:b}", 0b11001100);
+	test_eq("-11001100",   "{:b}", -0b11001100);
+	test_eq("0b11001100",  "{:#b}", 0b11001100);
+	test_eq("-0b11001100", "{:#b}", -0b11001100);
+	test_eq("0011001100",  "{:010b}", 0b11001100);
+	test_eq("  11001100",  "{:10b}", 0b11001100);
+
+
+	// octal
+
+	test_eq("1234567",    "{:o}", 01234567);
+	test_eq("01234567",   "{:#o}", 01234567);
+	test_eq("  01234567", "{:#10o}", 01234567);
+	test_eq("   1234567", "{:10o}", 01234567);
+	test_eq("  -1234567", "{:10o}", -01234567);
 }
+
 
 int main()
 {
 	test_common();
 
 	test_integer();
+
+	return errors_count;
 }
